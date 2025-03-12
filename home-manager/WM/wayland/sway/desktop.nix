8da3aca01fc5439629cf5578365369da5f83f7c6
Andreas V. W. Zacchi <zacchiandreas@gmail.com>
2025-03-12 19:55:04 +0100

    Change displays as they were renamed

--- home-manager/WM/wayland/sway/desktop.nix
+++ home-manager/WM/wayland/sway/desktop.nix
@@ -3,10 +3,10 @@
         config = {
             output = {
                 "*" = { bg = "#1d2021 solid_color"; };
-                DP-1 = {
+                DP-2 = {
                     position = "0 0";
                 };
-                DP-3 = {
+                DP-1 = {
                     position = "1920 0";
                 };
             };
