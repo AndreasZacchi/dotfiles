aa7f0f84a2f7002e113743abbb8003b33b820834
Andreas V. W. Zacchi <zacchiandreas@gmail.com>
2025-03-19 22:05:52 +0100

    Rename from opengl to graphics

--- hosts/home/gpu.nix
+++ hosts/home/gpu.nix
@@ -6,7 +6,7 @@
     boot.blacklistedKernelModules = [ "nvidia" "nvidia_uvm" "nvidia_drm" "nvidia_modeset" ];
 
     # Enable hardware acceleration for Nouveau
-    hardware.opengl = {
+    hardware.graphics = {
         enable = true;
         extraPackages = with pkgs; [
             mesa
