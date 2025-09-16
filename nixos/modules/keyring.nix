6a9c6fa125f2b6810a8a49482333b1fc1c5c41e2
Andreas V. W. Zacchi <zacchiandreas@gmail.com>
2025-09-16 21:12:26 +0200

    Enabled PAM so keyring is authenticated on TTY login

--- nixos/modules/keyring.nix
+++ nixos/modules/keyring.nix
@@ -10,4 +10,10 @@
   #Enable keyring
   security.polkit.enable = true;
   services.gnome.gnome-keyring.enable = true;
+
+  security.pam.services = {
+    login = {
+      enableGnomeKeyring = true;
+    };
+  };
 }
