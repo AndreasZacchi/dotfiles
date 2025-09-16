23ccab061cfa6e1111704029db3bc60e62a18d7d
Andreas V. W. Zacchi <zacchiandreas@gmail.com>
2025-09-16 21:24:16 +0200

    Remove battery from desktop version of waybar

--- home-manager/WM/wayland/waybar/default.nix
+++ home-manager/WM/wayland/waybar/default.nix
@@ -1,78 +1,71 @@
-{
-    # This requires the following fonts to be installed:
-    #font-awesome, powerline-fonts, powerline-symbols
-    programs.waybar = {
-        enable = true;
-        style = builtins.readFile ./style.css;
-        settings = [{
-            layer = "top";
-            position = "top";
-            exclusive = true;
+{ lib, pkgs, ... }:
 
-            modules-left = [ "sway/workspaces" "sway/mode" "wlr/taskbar" ];
-            modules-center = [ "sway/window" ];
-            modules-right = [ "network" "custom/divider" "bluetooth" "custom/divider" "pulseaudio" "custom/divider" "battery" "custom/divider" "clock" ];
+let
+  isLaptop = builtins.pathExists "/sys/class/power_supply/BAT0";
 
-            "battery" = {
-                format = "{capacity}% {icon}";
-                format-charging = "{capacity}% ⚡";
-                format-icons = [""  ""  ""  ""  ""];
-            };
-            "clock" = {
-                format-alt = "{:%a, %d. %b  %H:%M}";
-            };
-            "sway/window" = {
-                max-length = 50;
-            };
-            "sway/workspaces" = {
-                disable-scroll = true;
-                all-outputs = false;
-            };
+  baseBar = {
+    layer = "top";
+    position = "top";
+    exclusive = true;
 
-            "wlr/taskbar" = {
-                "on-click" = "activate";
-            };
+    modules-left   = [ "sway/workspaces" "sway/mode" "wlr/taskbar" ];
+    modules-center = [ "sway/window" ];
 
-            "network" = {
-                # "interface" = "wlp2s0"; # (Optional) To force the use of this interface
-                "format-wifi" = "{essid} ({signalStrength}%) ";
-                "format-ethernet" = "{ifname} = {ipaddr}/{cidr} ";
-                "format-disconnected" = "Disconnected ⚠";
-                "interval" = 10;
-                "on-click" = "nm-connection-editor";
-            };
-            "bluetooth" = {
-                format = "{icon}";
-                format-alt = "{status} {icon}";
-                interval = 30;
-                format-icons = {
-                    "enabled" = "";
-                    "disabled" = "";
-                };
-                tooltip-format = "{}";
-            };
-            "pulseaudio" = {
-                #"scroll-step" = 1;
-                "format" = "{volume}% {icon}";
-                "format-bluetooth" = "{volume}% {icon}";
-                "format-muted" = "{volume}% ";
-                "format-icons" = {
-                    "headphones" = "";
-                    "handsfree" = "";
-                    "headset" = "";
-                    "phone" = "";
-                    "portable" = "";
-                    "car" = "";
-                    "default" = ["" ""];
-                };
-                "on-click" = "pavucontrol";
-            };
+    "clock" = { format-alt = "{:%a, %d. %b  %H:%M}"; };
+    "sway/window" = { max-length = 50; };
+    "sway/workspaces" = { disable-scroll = true; all-outputs = false; };
+    "wlr/taskbar" = { "on-click" = "activate"; };
 
-            "custom/divider" = {
-                format = " | ";
-                interval = "once";
-                tooltip = false;
-            };
-        }];
+    "network" = {
+      "format-wifi" = "{essid} ({signalStrength}%) ";
+      "format-ethernet" = "{ifname} = {ipaddr}/{cidr} ";
+      "format-disconnected" = "Disconnected ⚠";
+      "interval" = 10;
+      "on-click" = "nm-connection-editor";
     };
+    "bluetooth" = {
+      format = "{icon}";
+      format-alt = "{status} {icon}";
+      interval = 30;
+      format-icons = { enabled = ""; disabled = ""; };
+      tooltip-format = "{}";
+    };
+    "pulseaudio" = {
+      "format" = "{volume}% {icon}";
+      "format-bluetooth" = "{volume}% {icon}";
+      "format-muted" = "{volume}% ";
+      "format-icons" = {
+        headphones = ""; handsfree = ""; headset = "";
+        phone = ""; portable = ""; car = ""; default = [ "" "" ];
+      };
+      "on-click" = "pavucontrol";
+    };
+    "custom/divider" = { format = " | "; interval = "once"; tooltip = false; };
+  };
+
+  rightDesktop = {
+    modules-right = [ "network" "custom/divider" "bluetooth" "custom/divider"
+                      "pulseaudio" "custom/divider" "clock" ];
+  };
+
+  rightLaptop = {
+    modules-right = [ "network" "custom/divider" "bluetooth" "custom/divider"
+                      "pulseaudio" "custom/divider" "battery" "custom/divider" "clock" ];
+    "battery" = {
+      format = "{capacity}% {icon}";
+      format-charging = "{capacity}% ⚡";
+      format-icons = [ "" "" "" "" "" ];
+    };
+  };
+in
+{
+  programs.waybar = {
+    enable = true;
+    style = builtins.readFile ./style.css;
+
+    # Build a SINGLE bar by merging base + host-specific attrs
+    settings = [
+      (baseBar // (if isLaptop then rightLaptop else rightDesktop))
+    ];
+  };
 }
