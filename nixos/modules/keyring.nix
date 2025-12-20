28314d250856386481361195e3e100b68c60adde
Andreas V. W. Zacchi <zacchiandreas@gmail.com>
2025-12-20 20:08:06 +0100

    Make swaylock use u2f with password fallback

--- nixos/modules/keyring.nix
+++ nixos/modules/keyring.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }: 
+{ pkgs, lib, config, ... }: 
 {
   # Install necessary packages for keyring
   environment.systemPackages = with pkgs; [
@@ -36,7 +36,10 @@
       greetd.enableGnomeKeyring = true;
       login.u2fAuth = true;
       sudo.u2fAuth = true;
-      swaylock.u2fAuth = true;
+      swaylock = {
+        u2fAuth = true;
+        rules.auth.unix.order = config.security.pam.services.swaylock.rules.auth.u2f.order - 10;
+      };
       polkit-1.u2fAuth = true;
       greetd.u2fAuth = true;
     };
