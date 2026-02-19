34b62ff09954fc69bb88964c831147deeb04d6f3
Andreas V. W. Zacchi <zacchiandreas@gmail.com>
2026-02-19 11:19:25 +0100

    Add bash aliases

--- home-manager/packages/terminal/bash.nix
+++ home-manager/packages/terminal/bash.nix
@@ -6,8 +6,15 @@
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
     '';
 
-    #shellAliases = {
-    #  
-    #};
+    shellAliases = {
+      # Rebuilds
+      nrs-home   = "sudo nixos-rebuild switch --flake .#home";
+      nrs-laptop = "sudo nixos-rebuild switch --flake .#laptop";
+
+      # GC / Store maintenance
+      nix-roots = "sudo nix-store --gc --print-roots";
+      nix-gc    = "sudo nix-collect-garbage -d";
+      nix-opt   = "sudo nix-store --optimise";
+    };
   };
 }
