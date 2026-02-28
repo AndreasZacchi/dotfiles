1e197e3b1393ecfa005f51c6b811d9a7c574d620
Andreas V. W. Zacchi <zacchiandreas@gmail.com>
2026-02-28 14:37:35 +0100

    Lock linux to kernel 6.12 on desktop due to RTL8852au support

--- hosts/home/network.nix
+++ hosts/home/network.nix
@@ -1,4 +1,6 @@
-{config, ...}:
+{config, pkgs, ...}:
 {
     boot.extraModulePackages = with config.boot.kernelPackages; [ rtl8852au ];
+    hardware.usb-modeswitch.enable = true;
+    boot.kernelPackages = pkgs.linuxPackages_6_12; # Stuck on 6.12 for rtl8852au support for now
 }
