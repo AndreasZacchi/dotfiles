e01a1d9ee9c94c022c618e5d94c61011b861bf3e
Andreas V. W. Zacchi <zacchiandreas@gmail.com>
2026-02-28 14:45:54 +0100

    Make GDM show login on main monitor desktop

--- nixos/modules/gdm.nix
+++ nixos/modules/gdm.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, isLaptop, ... }:
 {
     services.xserver = { # Maybe not needed?
         enable = true;
@@ -25,4 +25,21 @@
         gnome-console
         nautilus
     ];
+
+    # Apply user monitor settings to login screenby copying the user's monitors.xml to GDM's config directory
+    systemd.services.applyUserMonitorSettings = lib.mkIf (!isLaptop) (
+        let
+            username = "andreaszacchi";
+            gdmConfigDir = "/var/lib/gdm/seat0/config";
+        in {
+            description = "Apply user monitor settings to GDM login screen";
+            after = [ "network.target" "systemd-user-sessions.service" "display-manager.service" ];
+            wantedBy = [ "multi-user.target" ];
+
+            serviceConfig = {
+            Type = "oneshot";
+            ExecStart = "${pkgs.bash}/bin/bash -c 'echo \"Applying user monitor settings to GDM login screen\" && mkdir -p ${gdmConfigDir} && echo \"Created ${gdmConfigDir} directory\" && [ \"/home/${username}/.config/monitors.xml\" -ef \"${gdmConfigDir}/monitors.xml\" ] || cp /home/${username}/.config/monitors.xml ${gdmConfigDir}/monitors.xml && echo \"Copied monitors.xml to ${gdmConfigDir}/monitors.xml\" && chown gdm:gdm ${gdmConfigDir}/monitors.xml && echo \"Changed ownership of monitors.xml to gdm\"'";
+            };
+        }
+    );
 }
