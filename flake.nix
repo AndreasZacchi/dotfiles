82896d64628063b78af2acf773187e76616da9ff
Andreas V. W. Zacchi <zacchiandreas@gmail.com>
2026-04-29 11:17:05 +0200

    Add work/school printer driver

--- flake.nix
+++ flake.nix
@@ -72,6 +72,8 @@
 
           ./nixos/modules/ollama.nix
 
+          ./nixos/modules/printer.nix
+
           # make home-manager as a module of nixos
           # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
           home-manager.nixosModules.home-manager
--- nixos/modules/printer.nix
+++ nixos/modules/printer.nix
@@ -1,6 +1,32 @@
-{pkgs, ...}:
+{ pkgs, ... }:
+
+let
+  myPrintPpd = pkgs.runCommand "myprint-ppd" { } ''
+    install -Dm444 ${./myPrint_PS.ppd} $out/share/cups/model/myPrint_PS.ppd
+  '';
+in
 {
-# Enable CUPS to print documents.
   services.printing.enable = true;
-  services.printing.drivers = [ pkgs.brlaser ];
+  services.printing.drivers = [
+    pkgs.brlaser
+    myPrintPpd
+  ];
+
+  hardware.printers = {
+    ensurePrinters = [
+      {
+        name = "myPrint";
+        location = "School printer";
+        deviceUri = "https://print.nextkbh.dk/RicohmyPrint/printers/ipp/print";
+
+        model = "myPrint_PS.ppd";
+
+        ppdOptions = {
+          "auth-info-required" = "username,password";
+        };
+      }
+    ];
+
+    ensureDefaultPrinter = "myPrint";
+  };
 }
